d3b78a6ff4d438114b98174d00434ffc29c0de86
Govinda Roy <govindaroy.ofc94@gmail.com>
2024-07-04 17:59:59 +0600

    Update

--- js/main.js
+++ js/main.js
@@ -1609,6 +1609,9 @@ $(window).resize(function () {
 $(window).scroll(function () {
   parallaxEffectColor();
   parallaxEffectBlackAndWhite();
+  let header_text = document.getElementById('header__text');
+  let value = window.scrollY;
+  header_text.style.marginTop = value * 0.45 + 'px';
 });
 
 document.addEventListener('readystatechange', function () {
